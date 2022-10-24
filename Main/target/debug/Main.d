ac9ca6160e335d0b779c878c6119f2a195530913
Nicholas-Kipkoech <nickey968@gmail.com>
2022-10-24 14:09:42 +0300

    chore :init rust app

---
+++ Main/target/debug/Main.d
@@ -0,0 +1,1 @@
+/home/kipkoech/Desktop/projects/Learning_Rust/Main/target/debug/Main: /home/kipkoech/Desktop/projects/Learning_Rust/Main/src/main.rs
---
+++ Main/target/debug/deps/Main-25f34f1d008bd324.d
@@ -0,0 +1,5 @@
+/home/kipkoech/Desktop/projects/Learning_Rust/Main/target/debug/deps/Main-25f34f1d008bd324: src/main.rs
+
+/home/kipkoech/Desktop/projects/Learning_Rust/Main/target/debug/deps/Main-25f34f1d008bd324.d: src/main.rs
+
+src/main.rs:
