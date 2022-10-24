d6a21d15fabee1fba233fb5ca4a49f21b754f34a
Nicholas-Kipkoech <nickey968@gmail.com>
2022-10-24 14:22:24 +0300

    chore:simple guessing game

---
+++ geussing_game/target/debug/deps/geussing_game-7e617a3acfc393d1.d
@@ -0,0 +1,5 @@
+/home/kipkoech/Desktop/projects/Learning_Rust/geussing_game/target/debug/deps/geussing_game-7e617a3acfc393d1: src/main.rs
+
+/home/kipkoech/Desktop/projects/Learning_Rust/geussing_game/target/debug/deps/geussing_game-7e617a3acfc393d1.d: src/main.rs
+
+src/main.rs:
---
+++ geussing_game/target/debug/geussing_game.d
@@ -0,0 +1,1 @@
+/home/kipkoech/Desktop/projects/Learning_Rust/geussing_game/target/debug/geussing_game: /home/kipkoech/Desktop/projects/Learning_Rust/geussing_game/src/main.rs
